02ff1a6b0029ddca9502766befe9a35d7bc8be32
Hurricanezwf <1094646850@qq.com>
2018-08-14 23:02:17 +0800

    update

--- env.sh
+++ env.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 
-export K8S_HOME=/root/k8s
+K8S_SCRIPTS_HOME=/root/k8s-scripts
 
-export KUBELET_HOME=$K8S_HOME/kubelet
+K8S_BIN_HOME=/root/k8s-bin
 
-export SHARED_HOME=$K8S_HOME/shared
+K8S_DATA_HOME=/root/k8s-data
+
+KUBELET_HOME=$K8S_SCRIPTS_HOME/kubelet
+
+SHARED_HOME=$K8S_DATA_HOME/shared
--- kubelet/kubelet.sh
+++ kubelet/kubelet.sh
@@ -6,30 +6,35 @@ source ../env.sh
 
 
 # 存放证书的目录
-__CERT_DIR__=$SHARED_HOME/certs
-
-# Cgroup Driver
-__CGROUP_DRIVER__=systemd
+__CERT_DIR__=${SHARED_HOME}/certs
 
 # 当前主机一个网卡的IP
 __HOST_IP__=`ifconfig|grep '192.168.'|awk '{print $2}'`
+
+# 管理kubelet文件的目录
+__KUBELET_ROOT_DIR__=${K8S_DATA_HOME}/kubelet-root
 
 
 
 
 ## 准备启动环境
 function prepare(){
+	# 关闭swap分区
 	swapoff -a
+
+	# 创建目录
+	mkdir -p ${__CERT_DIR__}
+	mkdir -p ${__KUBELET_ROOT_DIR__}
 }
 
 ## 启动kubelet
 function start(){
 	prepare
 	kubelet \
+		--config=./kubelet-config.yaml \
 		--cert-dir=${__CERT_DIR__} \
-		--cgroup-driver=${__CGROUP_DRIVER__} \
 		--node-ip=${__HOST_IP__} \
-		--root-dir=${KUBELET_HOME}
+		--root-dir=${__KUBELET_ROOT_DIR__}
 
 	#--bootstrap-kubeconfig=TODO
 	#--cluster-domain=TODO
