bb2aca04550ced51a0c2233ad0cecedc5ec30272
zwf <1094646850@qq.com>
2018-08-28 09:49:23 +0800

    配置etcd

--- etcd/etcd.sh
+++ etcd/etcd.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+set -ex
+
+source ${K8S_SCRIPTS_HOME}/env.sh
+
+
+__etcd_name__="etcd-0"
+__etcd_root__=${K8S_DATA_HOME}/${__etcd_name__}
+
 
 function start(){
-	etcd --config-file=$K8S_SCRIPTS_HOME/etcd/etcd-config.yaml
+	mkdir -p ${__etcd_root__}
+
+	etcd \
+		--debug=true \
+		--enable-pprof=true \
+		--name=${__etcd_name__} \
+		--data-dir=${__etcd_root__}/data/ \
+		--wal-dir=${__etcd_root__}/wal/ \
+		--listen-peer-urls=https:\/\/${__LOCAL_ADVERTISE_IP__}:2380 \
+		--listen-client-urls=https:\/\/${__LOCAL_ADVERTISE_IP__}:2379 \
+		--initial-advertise-peer-urls=https:\/\/${__LOCAL_ADVERTISE_IP__}:2380 \
+		--advertise-client-urls=https:\/\/${__LOCAL_ADVERTISE_IP__}:2379 \
+		--cert-file=${__CERT_DIR__}/etcd.pem \
+		--key-file=${__CERT_DIR__}/etcd-key.pem \
+		--client-cert-auth=true \
+		--trusted-ca-file=${__CERT_DIR__}/etcd-ca.pem \
+		--peer-cert-file=${__CERT_DIR__}/etcd.pem \
+		--peer-key-file=${__CERT_DIR__}/etcd-key.pem \
+		--peer-client-cert-auth=true \
+		--peer-trusted-ca-file=${__CERT_DIR__}/etcd-ca.pem
 }
 
 
